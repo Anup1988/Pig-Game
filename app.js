af03f6697f1b130dc098609a842816af787c2ed9
Anup Prasai <prasai.anup@hotmail.com>
2019-08-05 02:25:45 -0400

    DRY principle: Game initialization function

--- app.js
+++ app.js
@@ -9,14 +9,7 @@ GAME RULES:
 
 var scores, roundScore, activePlayer;
 
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
-
-document.getElementById("score-0").textContent = 0;
-document.getElementById("score-1").textContent = 0;
-document.getElementById("current-0").textContent = 0;
-document.getElementById("current-1").textContent = 0;
+init();
 
 document.querySelector(".dice").style.display = "none";
 
@@ -50,11 +43,11 @@ document.querySelector(".btn-hold").addEventListener("click", function() {
     scores[activePlayer];
   //Check if the player won the game
   if (scores[activePlayer] >= 100) {
-    document.querySelector("#name-" + activePlayer).textContent = "Winner1";
+    document.querySelector("#name-" + activePlayer).textContent = "Winner!";
     document.querySelector(".dice").style.display = "none";
     document
       .querySelector(".player-" + activePlayer + "-panel")
-      .classList.add("Winner");
+      .classList.add("winner");
     document
       .querySelector(".player-" + activePlayer + "-panel")
       .classList.remove("active");
@@ -73,4 +66,23 @@ function nextPlayer() {
   document.querySelector(".player-0-panel").classList.toggle("active");
   document.querySelector(".player-1-panel").classList.toggle("active");
   document.querySelector(".dice").style.display = "none";
+}
+
+document.querySelector(".btn-new").addEventListener("click", init);
+
+function init() {
+  scores = [0, 0];
+  activePlayer = 0;
+  roundScore = 0;
+  document.getElementById("score-0").textContent = 0;
+  document.getElementById("score-1").textContent = 0;
+  document.getElementById("current-0").textContent = 0;
+  document.getElementById("current-1").textContent = 0;
+  document.getElementById("name-0").textContent = "Player 1";
+  document.getElementById("name-1").textContent = "Player 2";
+  document.querySelector(".player-0-panel").classList.remove("winner");
+  document.querySelector(".player-1-panel").classList.remove("winner");
+  document.querySelector(".player-0-panel").classList.remove("active");
+  document.querySelector(".player-0-panel").classList.add("active");
+  document.querySelector(".player-1-panel").classList.remove("active");
 }
